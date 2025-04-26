9ad69ab0c76df852670f5538028994a3e0d6bfd5
pisceslaffy <pisceslaffy@gmail.com>
2025-04-26 11:49:48 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -7,7 +7,7 @@ st.image("pangan.jpg", caption="Hello,welocome to *CalorieCounting!*")
 st.title("🔢CalorieCounting🔢")
 
 # Description
-st.write("Aplikasi atau panduan ini memungkinkan pengguna untuk mencatat asupan makanan mereka, melihat perkiraan jumlah kalori, dan mendapatkan rekomendasi menu harian yang seimbang berdasarkan prinsip 4 Sehat 5 Sempurna dan target kalori mereka. Ini membantu memastikan bahwa meskipun pengguna fokus pada pengelolaan kalori, mereka tetap mendapatkan nutrisi esensial yang dibutuhkan oleh tubuh." :🙋‍♀️:")
+st.write("Aplikasi atau panduan ini memungkinkan pengguna untuk mencatat asupan makanan mereka, melihat perkiraan jumlah kalori, dan mendapatkan rekomendasi menu harian yang seimbang berdasarkan prinsip 4 Sehat 5 Sempurna dan target kalori mereka. Ini membantu memastikan bahwa meskipun pengguna fokus pada pengelolaan kalori, mereka tetap mendapatkan nutrisi esensial yang dibutuhkan oleh tubuh.")
 with st.sidebar:
     st.header("Input Parameter")
     ion_type = st.radio("Pilih jenis ion yang diketahui:", ("[H⁺]", "[OH⁻]"))
